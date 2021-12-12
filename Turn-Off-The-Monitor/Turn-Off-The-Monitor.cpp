65720e241cf1af3cf3e49186bd014024e7b9f41b
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2021-12-12 23:06:19 +0100

    Turn-Off-The-Monitor

---
+++ Turn-Off-The-Monitor/Turn-Off-The-Monitor.cpp
@@ -0,0 +1,16 @@
+﻿//Creator Szymon "Imeldushiii" Kubiak
+#include <iostream>
+#include <Windows.h>
+int main()
+{
+    LRESULT zmienna = SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+    //HWND_BROADCAST - sending to all higher level windows
+    if (zmienna == true) {
+        std::cout << "[+] Turn Off Monitor" << std::endl;
+        Sleep(5000); //Turn on monitor Again
+        SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1);
+    }
+    else {
+        std::cout << "[-] Something Wrong!" << std::endl;
+    }
+}
